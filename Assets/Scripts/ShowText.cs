0a0f36654b8819ac9ed4aaeaaa3665f606d2469d
chuckborisnorris <114290567+chuckborisnorris@users.noreply.github.com>
2023-02-04 00:50:59 +0100

    Add files via upload

---
+++ Assets/Scripts/ShowText.cs
@@ -0,0 +1,31 @@
+﻿ using UnityEngine;
+ using UnityEngine.UI;
+ using System.Collections;
+ 
+ public class ShowText : MonoBehaviour {
+ 
+     public float letterPause = 0.2f;
+ 
+     public string message;
+//     public Text textComp;
+	 public GameObject bNo;
+	 public GameObject bYes;
+	 public GameObject textBox;
+ 
+     void Start () {
+     }
+	 
+	 public void Show ()  {
+		 textBox.GetComponentInChildren<Text>().text = message;
+		 bNo.SetActive(true);
+		 bYes.SetActive(true);
+		 textBox.SetActive(true);	 
+	 }
+ 
+//     IEnumerator TypeText() {
+//         foreach (char letter in message.ToCharArray()) {
+//             textComp.text += letter;
+//             yield return new WaitForSeconds (letterPause);
+//         }
+//     }
+ }
